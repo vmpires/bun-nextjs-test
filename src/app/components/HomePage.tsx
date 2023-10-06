c5aa6945221a14cd4c7d31fd8ee52682441bd16f
Victor Pires <vctprs@gmail.com>
2023-10-06 00:49:17 -0300

    add first message

---
+++ src/app/components/HomePage.tsx
@@ -0,0 +1,9 @@
+export default function HomePage() {
+    return (
+        <div>
+            <h1 className="font-sans text-4xl">
+                This is my first Bun project 4 fun & learn!
+            </h1>
+        </div>
+    )
+  }
